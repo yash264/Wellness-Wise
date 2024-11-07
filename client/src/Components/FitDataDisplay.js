d56fce27ae28bd1f9059c21a2845f6c1f85a6799
yash264 <yp20222068@gmail.com>
2024-11-07 18:44:40 +0530

    fix: error

--- client/src/Components/FitDataDisplay.js
+++ client/src/Components/FitDataDisplay.js
@@ -9,8 +9,8 @@ import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 const FitDataDisplay = () => {
 
   const [token, setToken] = useState([]);
-  const [values,setValues] = useState([]);
-  const [fitnessData, setFitnessData] = useState(null);
+  const [values, setValues] = useState([]);
+  const [fitnessData, setFitnessData] = useState([]);
 
   const navigate = useNavigate()
 
@@ -20,51 +20,67 @@ const FitDataDisplay = () => {
         const result = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo',
           {
             headers: {
-              "Authorization": `Bearer ${response.access_token}`,
+              Authorization: `Bearer ${response.access_token}`,
             }
           });
-          setValues(result.data);
+        console.log(result);
+        setValues(result.data);
         setToken(response.access_token);
       }
       catch (error) {
         console.log(error);
       }
-    }
+    },
+    scope: "https://www.googleapis.com/auth/fitness.activity.read",
   });
 
 
   const getFitnessData = async (token) => {
     try {
       console.log(token);
-      const response = await axios.post('https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
+      const response = await axios.post(
+        'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
         {
-          headers: {
-            Authorization: `Bearer ${token}`,
+          headers : {
+            Authorization : `Bearer  ${token}`,
+            'Content-Type' : 'application/json',
           },
-          body: JSON.stringify({     
-              aggregateBy: [{
-                dataSourceId:
-                  'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'
-              }],
-              bucketByTime: { 'durationMillis': 86400000 },
-              startTimeMillis: 1454284800000,
-              endTimeMillis: 1455062400000    
-            })
-    });
-    console.log(response);
-  }catch (error) {
-    console.log(error);
-  }
-};
+        },
+        {
+          aggregateBy: [
+            { dataTypeName: "com.google.step_count.delta" },
+            /*{ dataTypeName: "com.google.height" },
+            { dataTypeName: "com.google.weight" },
+            { dataTypeName: "com.google.heart_rate.bpm" }*/
+          ],
+          bucketByTime: { durationMillis: 86400000 }, 
+          startTimeMillis: Date.now() - 24 * 60 * 60 * 1000, 
+          endTimeMillis: Date.now()
+        },
+      );
+      console.log(response);
+      setFitnessData(response.data.bucket[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-return (
-  <div>
-    <h2>Google Fit Data</h2>
+  return (
+    <div>
+      <h2>Google Fit Data</h2>
 
-    <p>Name : {values.name}</p> 
-    <p>Email : {values.email} <img src={`${values.picture}`} style={{width:"35px",float:"right"}} /></p>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <p>Name : {values.name}</p>
+          </div>
+          <div class="col">
+            <p>Email : {values.email} <img src={`${values.picture}`} style={{ width: "35px", float: "right" }} /> </p>
+          </div>
+        </div>
+      </div>
 
-    {/*<GoogleLogin
+      {/*<GoogleLogin
       onSuccess={credentialResponse => {
         const response = jwtDecode(credentialResponse.credential);
         console.log(response);
@@ -73,14 +89,18 @@ return (
         console.log('Login Failed');
       }}
     />;*/}
-    <br/>
 
-    <button onClick={login} class="btn btn-outline-primary" >Login</button>
-    {token}
-    <button onClick={() => getFitnessData(token)}>Show</button>
+      <button onClick={login} class="btn btn-outline-primary" >Login</button>
+      <br/>
+      <br/>
+      <button onClick={() => getFitnessData(token)} class="btn btn-outline-danger">Fitness Data</button><br />
+        <div>
+          <h5>Step Count Data (Last 24 Hours) : {fitnessData.endTimeMillis/86400000 - fitnessData.startTimeMillis/86400000}</h5>
+        </div>
 
-  </div>
-);
+
+    </div>
+  );
 };
 
 export default FitDataDisplay;
